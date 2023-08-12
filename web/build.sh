83d6656465941ae8a25eda6bb343cab33d42e090
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-08-12 17:58:51 +0200

    Publish the website

--- web/build.sh
+++ web/build.sh
@@ -4,22 +4,22 @@ LANGUAGES="en fr"
 BASE=base
 INDEX=INDEX.all
 
-mkdir -p site
+mkdir -p ../docs
 
 echo "proceed to base files copy (support for languages: $LANGUAGES)"
-cp *.css site/
-cp *.png site/
-cp $BASE site/index.html
-sed -i "s#accesskey=\"[0-9]*\"##g" site/index.html
+cp *.css ../docs/
+cp *.png ../docs/
+cp $BASE ../docs/index.html
+sed -i "s#accesskey=\"[0-9]*\"##g" ../docs/index.html
 INC=$(cat $INDEX | tr "\n" " " | sed "s,\&,\\\&,g")
-sed -i "s#@@INCLUDE:PAGE@@#$INC#" site/index.html
-sed -i "s#@@LANG@@#all#g" site/index.html
-sed -i "s#\(id=\"contents\"\)#\1 style=\"visibility: hidden\"#" site/index.html
+sed -i "s#@@INCLUDE:PAGE@@#$INC#" ../docs/index.html
+sed -i "s#@@LANG@@#all#g" ../docs/index.html
+sed -i "s#\(id=\"contents\"\)#\1 style=\"visibility: hidden\"#" ../docs/index.html
 
 . ./lang.all
 
 for it in $lang; do
-  sed -i "s#__${it}__#$(eval echo \$__${it}__)#g" site/index.html
+  sed -i "s#__${it}__#$(eval echo \$__${it}__)#g" ../docs/index.html
 done
 
 
@@ -31,21 +31,21 @@ for LANG in $LANGUAGES; do
 
     echo "build ($LANG) page $it"
 
-    cp $BASE site/$it.$LANG.html
+    cp $BASE ../docs/$it.$LANG.html
     INC=$(cat $it.$LANG | tr "\n" " " | sed "s,\&,\\\&,g")
-    sed -i "s#@@INCLUDE:PAGE@@#$INC#" site/$it.$LANG.html
-    sed -i "s#\(@@INCLUDE:[^ ]*@@\)#\1\n#g" site/$it.$LANG.html
+    sed -i "s#@@INCLUDE:PAGE@@#$INC#" ../docs/$it.$LANG.html
+    sed -i "s#\(@@INCLUDE:[^ ]*@@\)#\1\n#g" ../docs/$it.$LANG.html
 
-    for inc in `grep "@@INCLUDE:.*@@" site/$it.$LANG.html | sed "s#.*@@INCLUDE:\(.*\)@@.*#\1#g"`; do
+    for inc in `grep "@@INCLUDE:.*@@" ../docs/$it.$LANG.html | sed "s#.*@@INCLUDE:\(.*\)@@.*#\1#g"`; do
       INC=$(cat $inc | tr "\n" " " | sed "s,\&,\\\&,g")
-      sed -i "s#@@INCLUDE:$inc@@#$INC#" site/$it.$LANG.html
+      sed -i "s#@@INCLUDE:$inc@@#$INC#" ../docs/$it.$LANG.html
     done
 
-    sed -i "s#@@LANG@@#$LANG#g" site/$it.$LANG.html
+    sed -i "s#@@LANG@@#$LANG#g" ../docs/$it.$LANG.html
 
     for it2 in $lang; do
-      sed -i "s#__${it2}__#$(eval echo \$__${it2}__)#g" site/$it.$LANG.html
-      sed -i "s#@@$it2@@#$it2#g" site/$it.$LANG.html
+      sed -i "s#__${it2}__#$(eval echo \$__${it2}__)#g" ../docs/$it.$LANG.html
+      sed -i "s#@@$it2@@#$it2#g" ../docs/$it.$LANG.html
     done
   done
 done
